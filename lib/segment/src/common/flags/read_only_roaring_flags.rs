502b3ec28e956dedb94e6f5b8a318d4ab6939bab
Daniel Boros <dancixx@gmail.com>
2026-06-05 22:44:28 +0200

    fix: reopen and update trues_count and falses_count

--- lib/segment/src/common/flags/read_only_roaring_flags.rs
+++ lib/segment/src/common/flags/read_only_roaring_flags.rs
@@ -164,9 +164,6 @@ impl<S: UniversalRead> LiveReload for ReadOnlyRoaringFlags<S> {
             // Possible optimization: If new_points is sorted, we should be able to use read_bit_range and iter_ones on top of it
             if self.storage.get_bit(u64::from(point))?.unwrap_or(false) {
                 self.bitmap.insert(point);
-            } else {
-                // Now false on disk: remove it, since the bitmap is patched, not rebuilt.
-                self.bitmap.remove(point);
             }
         }
 
